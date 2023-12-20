d330527da11e3f6eb892df0f0958d8500c0c464f
yuanqichau <yuanqi.chau@lionsbot.com>
2023-12-21 01:29:36 +0800

    feat: add rendering of robot on map.

--- include/map_render_engine/render_mapping_screen.hpp
+++ include/map_render_engine/render_mapping_screen.hpp
@@ -1,7 +1,4 @@
 // ROS Includes
-#include <message_filters/subscriber.h>
-#include <message_filters/sync_policies/approximate_time.h>
-#include <message_filters/synchronizer.h>
 #include <nav_msgs/OccupancyGrid.h>
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
@@ -16,33 +13,29 @@
 class MappingScreenRenderer {
 
 public:
-  ////////////////////////////////////////////////////////////////
-  // Constructor
+
   MappingScreenRenderer();
 
-  ////////////////////////////////////////////////////////////////
-
 private:
-  ////////////////////////////////////////////////////////////////
-  // Callback Functions
-  void synchronizedCallback(const nav_msgs::OccupancyGrid::ConstPtr &msg_map,
-                            const sensor_msgs::LaserScan::ConstPtr &msg_laser);
+
+  void mapCallback(const nav_msgs::OccupancyGrid::ConstPtr &msg_map);
 
   void timerCallback(const ros::TimerEvent &event);
 
-  ////////////////////////////////////////////////////////////////
-  // ROS
+  // Others
+  cv::Mat m_map_render;
+  cv::Mat m_robot_icon;
+  cv::Mat m_robot_icon_rotated;
 
+  // ROS
   // ROS Node Handles
   ros::NodeHandle m_nh, m_nh_private;
 
+  // ROS Node Parameters
+  std::string m_robot_icon_path;
+
   // ROS Subscribers
-  typedef message_filters::sync_policies::ApproximateTime<
-      nav_msgs::OccupancyGrid, sensor_msgs::LaserScan>
-      MappingScreenSyncPolicy;
-  message_filters::Subscriber<nav_msgs::OccupancyGrid> m_subscriber_map;
-  message_filters::Subscriber<sensor_msgs::LaserScan> m_subscriber_laser;
-  message_filters::Synchronizer<MappingScreenSyncPolicy> m_synchronizer;
+  ros::Subscriber m_subscriber_map;
 
   // ROS Publishers
   ros::Publisher m_publisher_rendered_image;
@@ -50,8 +43,10 @@ private:
   // ROS Timer
   ros::Timer m_timer_publish;
 
-  ////////////////////////////////////////////////////////////////
   // Others
-  cv::Mat m_map_render;
-  ////////////////////////////////////////////////////////////////
+  tf2_ros::Buffer m_tf_buffer;
+  tf2_ros::TransformListener m_tf_listener{m_tf_buffer};
+  geometry_msgs::TransformStamped m_robot_to_map_tf;
+  nav_msgs::MapMetaData m_map_meta_data;
+
 };
--- src/render_mapping_screen.cpp
+++ src/render_mapping_screen.cpp
@@ -1,16 +1,32 @@
 #include "map_render_engine/render_mapping_screen.hpp"
 
+void rotateImage(cv::Mat& input, cv::Mat& output, double angle)
+{
+  const cv::Point2f center(input.cols / 2.0, input.rows / 2.0);
+  const cv::Mat rotationMatrix = cv::getRotationMatrix2D(center, angle, 1.0);
+
+  cv::warpAffine(input, output, rotationMatrix, input.size());
+}
+
 MappingScreenRenderer::MappingScreenRenderer()
-    : m_nh_private{"~"}, m_synchronizer(MappingScreenSyncPolicy(10),
-                                        m_subscriber_map, m_subscriber_laser) {
+    : m_nh_private{"~"} {
+  // ROS Node Parameters
+  m_robot_icon_path = m_nh_private.param<std::string>("robot_icon", "robot.png");
+
   // Variables Initialization
   m_map_render = cv::Mat(300, 300, CV_8UC3);
+  m_robot_icon = cv::imread(m_robot_icon_path);
+  m_robot_icon_rotated = cv::imread(m_robot_icon_path);
+  // m_robot_icon = cv::imread(m_robot_icon_path, cv::ImreadModes::IMREAD_UNCHANGED);
+  // m_robot_icon_rotated = cv::imread(m_robot_icon_path, cv::ImreadModes::IMREAD_UNCHANGED);
+
+#ifdef DEBUG_MODE
+  cv::imshow("Robot Icon", m_robot_icon);
+  cv::waitKey(2);
+#endif
 
   // ROS Subscribers
-  m_subscriber_map.subscribe(m_nh, "/map", 10);
-  m_subscriber_laser.subscribe(m_nh, "/scan", 10);
-  m_synchronizer.registerCallback(
-      boost::bind(&MappingScreenRenderer::synchronizedCallback, this, _1, _2));
+  m_subscriber_map = m_nh.subscribe<nav_msgs::OccupancyGrid>("/map", 1, &MappingScreenRenderer::mapCallback, this);
 
   // ROS Publishers
   m_publisher_rendered_image =
@@ -23,9 +39,8 @@ MappingScreenRenderer::MappingScreenRenderer()
   ros::spin();
 }
 
-void MappingScreenRenderer::synchronizedCallback(
-    const nav_msgs::OccupancyGrid::ConstPtr &msg_map,
-    const sensor_msgs::LaserScan::ConstPtr &msg_laser) {
+void MappingScreenRenderer::mapCallback(
+    const nav_msgs::OccupancyGrid::ConstPtr &msg_map) {
   ////////////////////////////////////////////////////////////////
   // If Map Image Size Is Different, Resize Render
 
@@ -52,6 +67,7 @@ void MappingScreenRenderer::synchronizedCallback(
   std::uint8_t *p; // unsigned char pointer to access value
   for (std::size_t i = 0; i < rows; ++i) {
     // flip image at the same time in axis 0
+    // to account for Occupancy Grid & Image Axis Difference
     p = m_map_render.ptr<std::uint8_t>(rows - i - 1);
     for (size_t j = 0; j < cols; ++j) {
       std::int8_t current_data = msg_map->data[i * cols + j];
@@ -65,7 +81,7 @@ void MappingScreenRenderer::synchronizedCallback(
       // Known Pixels, Set to 0-255 based on intensity
       else {
         // Probability to Pixel Value Conversion
-        constexpr float scale_factor = 255. / 100.;
+        static constexpr float scale_factor = 255. / 100.;
         p[j * 3 + 0] =
             255 - static_cast<std::uint8_t>(current_data * scale_factor);
         p[j * 3 + 1] =
@@ -82,10 +98,84 @@ void MappingScreenRenderer::synchronizedCallback(
 #endif
 
   ////////////////////////////////////////////////////////////////
+  // Update Map Info
+  m_map_meta_data = msg_map->info;
+
+  if (m_robot_icon.cols != static_cast<int>(0.5/m_map_meta_data.resolution) || m_robot_icon.rows != static_cast<int>(0.5/m_map_meta_data.resolution))
+  {
+    cv::resize(m_robot_icon, m_robot_icon, cv::Size(0.5/m_map_meta_data.resolution, 0.5/m_map_meta_data.resolution));
+    cv::resize(m_robot_icon_rotated, m_robot_icon_rotated, cv::Size(0.5/m_map_meta_data.resolution, 0.5/m_map_meta_data.resolution));  
+  }
+
+#ifdef DEBUG_MODE
+  cv::imshow("Robot Icon", m_robot_icon);
+  cv::waitKey(2);
+#endif
+
+  ////////////////////////////////////////////////////////////////
+
 }
 
 void MappingScreenRenderer::timerCallback(const ros::TimerEvent &event) {
-  // std::cout << "Timer Callback\n";
+
+  ////////////////////////////////////////////////////////////////
+  // Get Robot to Map Transformation
+  try
+  {
+      m_robot_to_map_tf = m_tf_buffer.lookupTransform("map", "base_link", ros::Time::now(), ros::Duration(1.0));
+  }
+  catch (tf2::TransformException &ex)
+  {
+      ROS_WARN("%s", ex.what());
+      return;
+  }
+
+  // get x, y, yaw
+  double x = m_robot_to_map_tf.transform.translation.x;
+  double y = m_robot_to_map_tf.transform.translation.y;
+  tf2::Quaternion rotation_quaternion(
+    m_robot_to_map_tf.transform.rotation.x,
+    m_robot_to_map_tf.transform.rotation.y,
+    m_robot_to_map_tf.transform.rotation.z,
+    m_robot_to_map_tf.transform.rotation.w
+  );
+  tf2::Matrix3x3 rotation_matrix(rotation_quaternion);
+  double roll, pitch, yaw;
+  rotation_matrix.getRPY(roll, pitch, yaw);
+
+#ifdef DEBUG_MODE
+  ROS_INFO("Robot To Map TF (x, y, yaw): (%lf, %lf, %lf)", x, y, yaw);
+#endif
+
+  ////////////////////////////////////////////////////////////////
+  // Rotate Robot Icon by Yaw
+  static constexpr double rads_to_degrees = 180./3.14159;
+  rotateImage(m_robot_icon, m_robot_icon_rotated, yaw*rads_to_degrees);
+
+#ifdef DEBUG_MODE
+  cv::imshow("Robot Icon Rotated", m_robot_icon_rotated);
+  cv::waitKey(2);
+#endif
+
+  ////////////////////////////////////////////////////////////////
+  // Calculate Robot Top Left Corner in Image
+  double x_image;
+  double y_image;
+  x_image = (x - m_map_meta_data.origin.position.x) / m_map_meta_data.resolution;
+  y_image = (y - m_map_meta_data.origin.position.y) / m_map_meta_data.resolution;
+  x_image = std::round(x_image - m_robot_icon_rotated.cols/2);
+  y_image = std::round((m_map_meta_data.height - y_image)  - m_robot_icon_rotated.rows/2);
+
+  cv::Rect roi(x_image, y_image, m_robot_icon_rotated.cols, m_robot_icon_rotated.rows);
+  std::cout << "Roi: " << roi << "\n";
+  std::cout << "robot_icon: " << m_robot_icon.size() << "\n";
+  m_robot_icon_rotated.copyTo(m_map_render(roi));
+
+#ifdef DEBUG_MODE
+  cv::imshow("Final Map Render", m_map_render);
+  cv::waitKey(1);
+#endif
+
 }
 
 int main(int argc, char **argv) {
